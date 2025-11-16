19d1359a4de8274776983ed4172a0a43feae7993
Łukasz Stępień <lukaszatepien@gmail.com>
2025-11-16 11:38:25 +0100

    Preparing code for jwt

--- backend/src/main/java/com/lokummeet/backend/entity/User.java
+++ backend/src/main/java/com/lokummeet/backend/entity/User.java
@@ -1,21 +1,29 @@
 package com.lokummeet.backend.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+@Getter
+@Setter
 @Table(name = "users")
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
-    private int id;
+    private Long id;
     @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false, unique = true)
@@ -42,6 +50,37 @@ public class User {
     @Column(name = "updated_at")
     @LastModifiedDate
     private OffsetDateTime updatedAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordHash;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
 enum UserRoles { ROLES_USER, ROLE_INSTITUTION, ROLE_ADMIN }
---
+++ backend/src/main/java/com/lokummeet/backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lokummeet.backend.repository;
+
+import com.lokummeet.backend.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
