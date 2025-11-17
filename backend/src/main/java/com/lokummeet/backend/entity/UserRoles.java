ac1973fc0b366b1e60947c9c8df4182a16881ec6
Łukasz Stępień <lukaszatepien@gmail.com>
2025-11-17 08:28:23 +0100

    Change ROLES_USER to ROLE_USER in java UserRoles

--- backend/src/main/java/com/lokummeet/backend/entity/UserRoles.java
+++ backend/src/main/java/com/lokummeet/backend/entity/UserRoles.java
@@ -3,7 +3,7 @@ package com.lokummeet.backend.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum UserRoles implements GrantedAuthority {
-    ROLES_USER,
+    ROLE_USER,
     ROLE_INSTITUTION,
     ROLE_ADMIN ;
 
