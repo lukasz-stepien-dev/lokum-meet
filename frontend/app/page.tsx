d4a1c638a1725bac29c3f41b51f3af15c5f673c6
lukasz-stepien-dev <lukaszatepien@gmail.com>
2025-12-16 15:10:53 +0100

    dashboard refactor

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,67 @@
 import Image from "next/image";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {Item, ItemActions, ItemContent, ItemDescription, ItemHeader, ItemTitle} from "@/components/ui/item";
+import {Calendar} from "lucide-react";
+import {Button} from "@/components/ui/button";
+
+const example = [
+    {
+        name: "Film club",
+        description: "Everyday tasks and UI generation.",
+        image:
+            "https://images.unsplash.com/photo-1650804068570-7fb2e3dbf888?q=80&w=640&auto=format&fit=crop",
+    },
+    {
+        name: "Sport club",
+        description: "Advanced thinking or reasoning.",
+        image:
+            "https://images.unsplash.com/photo-1610280777472-54133d004c8c?q=80&w=640&auto=format&fit=crop",
+    },
+    {
+        name: "Book club",
+        description: "Open Source model for everyone.",
+        image:
+            "https://images.unsplash.com/photo-1602146057681-08560aee8cde?q=80&w=640&auto=format&fit=crop",
+    },
+]
+
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-
-      </main>
-    </div>
+  <main className="flex min-h-screen w-full flex-col">
+      <h1 className={"text-4xl mt-10 ml-7 mb-3"}>For You</h1>
+    <Carousel
+        opts={{
+            align: "start"
+        }}
+        className={"w-full"}
+    >
+        <CarouselContent className={"px-7"}>
+            <CarouselItem className={"basis-1/6"}>
+                <Item variant={"outline"}>
+                    <ItemHeader>
+                        <Image
+                            src={example[0].image}
+                            alt={example[0].description}
+                            width={300} height={150}
+                            className={"aspect-video object-cover w-full rounded-md"}
+                        />
+                    </ItemHeader>
+                    <ItemContent className={"w-full"}>
+                        <ItemTitle className={"text-primary pl-1 text-xl"}>Tue, Jan 5, 5:00 PM</ItemTitle>
+                        <ItemTitle className={"text-2xl truncate pl-1"}>{example[0].name}</ItemTitle>
+                        <ItemDescription className={"pl-1"}>{example[0].description}</ItemDescription>
+                        <ItemActions className={"mt-4"}>
+                            <Button variant={"outline"} className={"w-1/2"}>Zapisz się</Button>
+                            <Button variant={"link"} className={"w-1/2"}>Więcej</Button>
+                        </ItemActions>
+                    </ItemContent>
+                </Item>
+            </CarouselItem>
+        </CarouselContent>
+    </Carousel>
+  </main>
   );
 }
--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -3,13 +3,15 @@ import {NextFont} from "next/dist/compiled/@next/font";
 import {Separator} from "@/components/ui/separator";
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Plus} from "lucide-react";
 
 const climate: NextFont = Climate_Crisis({
     subsets: ['latin'],
 });
 
 const Header = () => {
-  return (<header className={"flex w-full flex-col font-sans"}>
+  return (<header className={"flex w-full flex-col font-sans sticky top-0 backdrop-blur-2xl"}>
       <div className={"flex flex-row items-center"}>
           <h1 className={`${climate.className} text-primary text-3xl p-4 w-full`}>Lokum Meet</h1>
           <Input className={"mr-4 w-1/3"} />
@@ -17,6 +19,9 @@ const Header = () => {
               <AvatarImage />
               <AvatarFallback>AB</AvatarFallback>
           </Avatar>
+          <Button className={"mr-4"}>
+              <Plus /> Stwórz
+          </Button>
       </div>
       <Separator  />
   </header>);
