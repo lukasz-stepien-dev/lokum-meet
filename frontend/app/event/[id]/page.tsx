2e8506692deccb2dd4a3e087f2cc67e09f653ed8
Łukasz Stępień <lukaszatepien@gmail.com>
2026-03-18 16:35:40 +0100

    #11 Add date on event page and check isError

--- frontend/app/event/[id]/page.tsx
+++ frontend/app/event/[id]/page.tsx
@@ -4,13 +4,16 @@ import useEventId from "@/hooks/useEventId";
 import { Button } from "@/components/ui/button";
 import { ArrowUpLeft } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useFormatter } from "next-intl";
 
 export default function Page() {
   const { id } = useParams<{ id: string }>()
   const { event, isLoading, isError } = useEventId(Number(id));
   const router = useRouter();
+  const format = useFormatter();
+  if (isError || event === undefined) return <div>Błąd załadowania danych</div>;
   return (
-    // #TODO: implement event page
+    // #TODO: implement photo
     <section className={"flex justify-center w-full pt-10"}>
       <section className={""}>
         <Button
@@ -24,32 +27,35 @@ export default function Page() {
           Powrót
         </Button>
 
-        <h1 className={"text-4xl font-extrabold tracking-tight text-balance"}>{event?.title}</h1>
+        <h1 className={"text-4xl font-extrabold tracking-tight text-balance"}>
+          {event.title}
+        </h1>
+        <h2>
+          {
+            format.dateTime(new Date(event.startTime), {
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            })
+          }
+        </h2>
         <section className={"flex flex-row items-center my-5"}>
           <Avatar>
             <AvatarImage
-              src={event?.createdBy.avatarUrl}
+              src={event.createdBy.avatarUrl}
               alt={"Author avatar"}
-              />
+            />
             <AvatarFallback>
-              {
-                event?.createdBy.username.substring(0, 2).toUpperCase()
-              }
+              {event.createdBy.username.substring(0, 2).toUpperCase()}
             </AvatarFallback>
           </Avatar>
-          <p className={"leading-7 ml-3"}>
-            {
-              event?.createdBy.username
-            }
-          </p>
+          <p className={"leading-7 ml-3"}>{event.createdBy.username}</p>
         </section>
 
-
-        <p>
-          {
-            event?.description
-          }
-        </p>
+        <p>{event.description}</p>
       </section>
     </section>
   );
